f90ffe4857a35213ebec363740fe7197f143de90
soura49 <soura_balakrishna49@yahoo.com>
2018-01-08 08:04:25 -0500

    Add files via upload

---
+++ IntroductionToPython/lamdba_function.py
@@ -0,0 +1,12 @@
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
+def lamdba_handler():
+    print datetime.today().date()
+
+
+
+lamdba_handler()
