130a61247329697e8b145b2421d340fba3b4c8d7
soura49 <soura_balakrishna49@yahoo.com>
2018-09-05 19:44:20 -0400

    Update describe_instances.py

--- describe_instances.py
+++ describe_instances.py
@@ -5,7 +5,7 @@ from botocore.exceptions import ClientError
 ec2 = boto3.client('ec2')
 mylist = []
 def get_instances():
-    filters = [{ 'Name': 'tag:Owner','Values': [ 'balakrishna' ] }]
+    filters = [{ 'Name': 'tag:Owner','Values': [ sys.argv[1]] }]
     response = ec2.describe_instances(Filters = filters)
     #print response
     for reservation in response["Reservations"]:
